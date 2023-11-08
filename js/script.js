7dc3b1be9b6bed94a6505c5fe7288d8f73715968
n1k-1ta <n1k_1ta@bk.ru>
2023-11-09 00:21:16 +0500

    commit 3

--- js/script.js
+++ js/script.js
@@ -1,38 +1,30 @@
-    // Функция для добавления новой формы
-    function addForm() {
-        var formsContainer = document.getElementById('forms_container');
-        var newForm = document.createElement('form');
-        newForm.className = 'main_form';
-        var label = document.createElement('label');
-        label.setAttribute('for', 'deputy_editor');
-        label.className = 'label_subtitle';
-        label.textContent = 'Заместители председателя';
-        var input = document.createElement('input');
-        input.setAttribute('type', 'text');
-        input.setAttribute('id', 'deputy_editor');
-        var deleteButton = document.createElement('button');
-        deleteButton.className = 'delete_button';
-        deleteButton.textContent = 'Удалить';
-        deleteButton.onclick = function() {
-            removeForm(this);
-        };
-
-        newForm.appendChild(label);
-        newForm.appendChild(input);
-        newForm.appendChild(deleteButton);
-
-        formsContainer.appendChild(newForm);
-}
-
-      // Функция для удаления формы
-    function removeForm(button) {
-        var form = button.parentNode;
-        form.parentNode.removeChild(form);
-}
-
-// Добавление новой формы для второго типа
-function addMemberForm() {
+function addForm() {
     var formsContainer = document.getElementById('forms_container');
+    var newForm = document.createElement('form');
+    newForm.className = 'main_form';
+    var label = document.createElement('label');
+    label.setAttribute('for', 'deputy_editor');
+    label.className = 'label_subtitle';
+    label.textContent = 'Заместители председателя';
+    var input = document.createElement('input');
+    input.setAttribute('type', 'text');
+    input.setAttribute('id', 'deputy_editor');
+    var deleteButton = document.createElement('button');
+    deleteButton.className = 'delete_button';
+    deleteButton.textContent = 'Удалить';
+    deleteButton.onclick = function() {
+        removeForm(this);
+    };
+
+    newForm.appendChild(label);
+    newForm.appendChild(input);
+    newForm.appendChild(deleteButton);
+
+    formsContainer.appendChild(newForm);
+}
+
+function addMemberForm() {
+    var memberFormsContainer = document.getElementById('member_forms_container');
     var newForm = document.createElement('form');
     newForm.className = 'main_form';
     var label = document.createElement('label');
@@ -53,11 +45,14 @@ function addMemberForm() {
     newForm.appendChild(input);
     newForm.appendChild(deleteButton);
 
-    formsContainer.appendChild(newForm);
+    memberFormsContainer.appendChild(newForm);
 }
 
-// Удаление формы второго типа
+// Удаление формы
 function removeForm(button) {
     var form = button.parentNode;
     form.parentNode.removeChild(form);
 }
+
+
+
